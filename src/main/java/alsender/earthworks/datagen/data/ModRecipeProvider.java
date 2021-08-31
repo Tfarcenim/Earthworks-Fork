0c8fb68ed1fd168edf75196fcce6522696c04a15
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-08-31 19:07:28 -0400

    smelting recipes

--- src/main/java/alsender/earthworks/datagen/data/ModRecipeProvider.java
+++ src/main/java/alsender/earthworks/datagen/data/ModRecipeProvider.java
@@ -1,8 +1,13 @@
 package alsender.earthworks.datagen.data;
 
+import alsender.earthworks.datagen.data.builders.CookingRecipeBuilderFullStack;
+import alsender.earthworks.main.registry.ItemRegistry;
+import net.minecraft.data.CookingRecipeBuilder;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
 
 import java.util.function.Consumer;
 
@@ -16,6 +21,11 @@ public class ModRecipeProvider extends RecipeProvider {
 
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
-
+        CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(ItemRegistry.chalk_dust),ItemRegistry.quicklime,.1f,200)
+                .addCriterion("has_chalk_dust",hasItem(ItemRegistry.chalk_dust))
+                .build(consumer);
+        CookingRecipeBuilderFullStack.smeltingRecipe(Ingredient.fromItems(ItemRegistry.chalk),new ItemStack(ItemRegistry.quicklime,4),.4f,200)
+                .addCriterion("has_chalk",hasItem(ItemRegistry.chalk))
+                .build(consumer,"quicklime_from_chalk_dust");
     }
 }
